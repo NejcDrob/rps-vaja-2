695c9ead296fc8f2754e7b2a501b606262ab5974
nejcdrobnic <nejcdrob@gmail.com>
2023-03-31 13:20:33 +0200

    added function definitions

---
+++ vaja2.py
@@ -0,0 +1,22 @@
+def my_roberts(slika):
+    #vaša implementacija
+    slika_robov=0
+    return slika_robov 
+
+def my_prewitt(slika):
+    #vaša implementacija
+    slika_robov=0
+    return slika_robov 
+
+def my_sobel(slika):
+    #vaša implementacija
+    slika_robov=0
+    return slika_robov 
+
+def canny(slika, sp_prag, zg_prag):
+    #vaša implementacija
+    slika_robov=0
+    return slika_robov 
+
+
+img = cv2.imread("lenna.png",0)
